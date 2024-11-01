654734af9803398264d640b0fd837f2851391046
Smrithimadhu <smrithi.madhu1603@gmail.com>
2024-11-01 12:34:17 +0530

    updated today

--- src/app/registration/components/registration.component.ts
+++ src/app/registration/components/registration.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { RegistrationService } from '../service/registration.service';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [ ReactiveFormsModule, CommonModule ],
+  imports: [ ReactiveFormsModule, CommonModule, HttpClientModule ],
   templateUrl: './registration.component.html',
   styles: ``
 })
@@ -15,8 +17,12 @@ import { ToastrService } from 'ngx-toastr';
 export class RegistrationComponent {
   addUserForm: FormGroup;
   isSaved = false;
+  registrationSuccess = false;
+  registrationError = '';
+  userService: any;
+  
 
-  constructor(/*private toastr: ToastrService*/)  {
+  constructor(/*private toastr: ToastrService*/ /*private registraionService: RegistrationService*/)  {
     this.addUserForm = new FormGroup({
       name: new FormControl('', Validators.required), 
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -27,6 +33,7 @@ export class RegistrationComponent {
   handleAddUser(){
     if (this.addUserForm.valid) {
     console.log(this.addUserForm.value);
+    // TOASTR
     // send the data to the service
     // this.employeesService.addEmployee(this.addUserForm.value).subscribe((response: any) => {
     //   console.log(response);
@@ -36,9 +43,26 @@ export class RegistrationComponent {
     // } else {
     //   this.toastr.error('Please fill out the form correctly.', 'Error'); //Show error if form is invalid
     // }
-    }
+
+    //  INTERGRATION
+    // const user = this.addUserForm.value;
+    //   this.userService.registerUser(user).subscribe({
+    //     next: (response: any) => {
+    //       this.registrationSuccess = true;
+    //       this.registrationError = '';
+    //       console.log('Registration successful:', response);
+    //       // Optionally, redirect to a login page or show a success message.
+    //     },
+    //     error: (error: { error: { message: string; }; }) => {
+    //       this.registrationSuccess = false;
+    //       this.registrationError = error.error.message || 'Registration failed. Please try again.';
+    //       console.error('Registration error:', error);
+    //     }
+    //   });
+    // }
   }
 
+  // TOASTR
   // simulateServiceCall() {
   //   // Replace this with your actual service call
   //   setTimeout(() => {
@@ -46,6 +70,9 @@ export class RegistrationComponent {
   //     this.toastr.success('User registered successfully!', 'Success');
   //   }, 1000); // Simulate a 1-second delay
   // }
+
+  
+}
 }
 
 
